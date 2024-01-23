7c9ad285fc7a0cf2d03fb0e718fde2172d60a29d
parkie0517 <parkie0517@gmail.com>
2024-01-23 01:38:06 +0000

    Initialize the tokens for the experiment

--- ln.py
+++ ln.py
@@ -4,27 +4,30 @@ By running this code, you will learn how the LN works.
 Let's begin!
 """
 
-# Step 1
+
 """
+Step 1
 Let's Import the necessary libraries
 """
 import torch
 import torch.nn as nn
 
 
-# Step 2
-"""Define the number of tokens and its dimension"""
-num_tokens = 4 # number of tokens
-dim = 2 # embedding size of the tokens
+"""
+Step 2
+Initialize the tokens
+"""
+input_data = torch.tensor([[[0.1, -0.2], [0.3, 0.4], [-0.5, -0.6], [0.7, 0.8]]])
+num_batch, num_tokens, dim = input_data.shape
 
-# Step 3
-"""Initialize the tokens"""
-tokens = torch.randn(num_tokens, dim)
 
-# Step 4
-"""Define the LN layer"""
+"""
+Step 3
+Define the LN layer
+"""
 # normalized_shape is the input shape from the last dimension onwards that will be normalized.
-layer_norm = nn.LayerNorm(normalized_shape=dim)
+layer_norm = nn.LayerNorm(normalized_shape=dim) 
+
 
 # Optionally, set custom gamma (scale) and beta (shift) parameters
 # Here, both gamma and beta are of shape (2,) as our last dimension is of size 2
